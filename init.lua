1f50420104fd286c8bd19a911c86b318678dc684
Jack Mu <jackmu@umich.edu>
2025-04-04 00:45:47 -0700

    suppressed errors in cursor however leader key is not working

--- init.lua
+++ init.lua
@@ -82,6 +82,11 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+if vim.g.vscode then
+  require 'vscode-config.vscode'
+  return
+end
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
